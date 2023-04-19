8a7e0d58b783b540e9a6149b9710e56814cd5cb7
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-19 15:03:00 +0000

    task 2 fixed

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,10 @@
 export default function getNeighborhoodsList() {
-	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  /* eslint-disable */
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-	const self = this;
-	this.addNeighborhood = (newNeighborhood) => {
-		self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		return self.sanFranciscoNeighborhoods;
-	};
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
