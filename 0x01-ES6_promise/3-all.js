aaac6976bf6b7e1b43aa13d54799dd095210ed71
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-19 13:27:09 +0000

    task 3 added

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+export { uploadPhoto, createUser } from './utils'
+
+export default function handleProfileSignup() {
+  /* eslint-disable */
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => console.log('Signup system offline'));
+}
