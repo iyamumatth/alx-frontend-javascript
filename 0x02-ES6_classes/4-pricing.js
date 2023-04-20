f9b4055614bad5225ef15aeae5b7138e73a9c8c1
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-20 13:33:32 +0000

    task 4 eslint fixed?

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,8 +1,8 @@
-// eslint-disable-next-line no-unused-vars
 import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    /* eslint-disable */
     this._currency = currency;
     this._amount = amount;
   }
