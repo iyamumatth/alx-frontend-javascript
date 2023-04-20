19f965b1e826439a3ba10380962aee71034a3111
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-20 11:31:50 +0000

    task 1 added

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  /* eslint-disable */
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
