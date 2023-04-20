24ee998cfe264a6fbb8a234ade099b191ed8b50e
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-20 14:54:04 +0000

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,13 @@
 export default class Building() {
   /* eslint-disable */
-  constructor(sqft) {
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-	'Class extending Building must override evacuationWarningMessage',
-      );
-    }
+  constructor() {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
 
     this._sqft = sqft;
   }
 
-  // getter
+  // getter 
   get sqft() {
-    return this._sqft;
+    return this.sqft;
   }
 }
