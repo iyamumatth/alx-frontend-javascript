1098b00224f2f9aef1b5256e8660a43ec32b902c
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-20 13:40:23 +0000

    task 4 added

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,6 +1,7 @@
 import Currency from './3-currency';
 
 export default class Pricing {
+  /* eslint-disable */
   constructor(amount, currency) {
     this._currency = currency;
     this._amount = amount;
