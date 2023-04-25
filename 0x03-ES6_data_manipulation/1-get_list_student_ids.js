678d078fb4008453bbb5becbabcc8484075767d8
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-25 10:07:50 +0000

    task 0

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,5 +3,4 @@ export default function getListStudentIds(studentArray) {
   if (!Array.isArray(studentIdArray)) {
     return [];
   }
-  return studentIdArray.map((item) => item.id);
-}
+  return studentIdArray.m
