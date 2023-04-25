6308a60ff49d8ea35b464d3b77117c75a1b42b20
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-25 09:29:50 +0000

    task 0 added

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  /* eslint-diable */
+  const students = [
+    { id: 1, firstName: 'Guillame', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
