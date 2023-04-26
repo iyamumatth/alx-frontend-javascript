528a2e9de36e4dc318d3f58e0332d41c87008e31
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-26 15:08:15 +0000

    task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,48 @@
+// Interface of directors
+interface DirectorsInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string; 
+}
+
+// Interface of teachers
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Class director
+class Director implements DirectorsInterface {
+	workFromHome(): string {
+		return 'Working from home';
+	}
+	getCoffeeBreak(): string {
+		return 'Getting a coffee break';
+	}
+	workDirectorTasks(): string {
+		return 'Getting to director tasks';
+	}
+}
+
+// Teacher Class
+class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return 'Cannot work from home';
+	}
+	getCoffeeBreak(): string {
+		return 'Cannot have a break';
+	}
+	workTeacherTasks(): string {
+		return 'Getting work';
+	}
+}
+
+function createEmployee(firstName: string, lastName: string, salary: number | string): Director | Teacher {
+	if (salary as number && salary < 500) return new Teacher();
+	else return new Director();
+}
+
+	console.log(createEmployee('Harley', 'Searle', 200));
+	console.log(createEmployee('Alex', 'Ferguson', 1000));
+	console.log(createEmployee('Jacob', 'Zuma', '$500'));
